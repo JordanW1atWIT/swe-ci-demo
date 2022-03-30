d0a8c6600cb7bd27a1caca7a99d95348f4b9f5bb
JordanW1atWIT <71137820+JordanW1atWIT@users.noreply.github.com>
2022-03-30 14:48:24 -0400

    Update FizzBuzz.java
    Added Code

--- src/main/java/com/demo/FizzBuzz.java
+++ src/main/java/com/demo/FizzBuzz.java
@@ -4,14 +4,59 @@ public class FizzBuzz
 {
     String processNumber(int num)
 	{	
-    	// Copy your code here
-		return "";
+    	if(num == 0)
+          {
+            return String.valueOf(0);  
+          }
+          else if(num % 15 == 0)
+          {
+            return "fizz buzz";
+          }
+          else if(num % 5 == 0)
+          {
+            return "buzz";
+          }
+          else if(num % 3 == 0)
+          {
+            return "fizz";
+          }
+          else
+          {
+            return String.valueOf(num);
+          }
 	}
 	
 	String processNumbers (int[] numList)
 	{
-    	// Copy your code here
-		return "";
+    	String temp = "";
+    if(numList.length == 0)
+          {
+            return "invalid";
+          }
+        for(int i = 0; i < numList.length; i++)
+        {
+          if(numList[i] == 0)
+          {
+            temp += String.valueOf(0) + ",";  
+          }
+          else if(numList[i] % 15 == 0)
+          {
+            temp += "fizz buzz,";
+          }
+          else if(numList[i] % 5 == 0)
+          {
+            temp += "buzz,";
+          }
+          else if(numList[i] % 3 == 0)
+          {
+            temp += "fizz,";
+          }
+          else
+          {
+            temp += String.valueOf(numList[i]) + ",";
+          }
+        }
+        return temp;
 	}
 
 }
