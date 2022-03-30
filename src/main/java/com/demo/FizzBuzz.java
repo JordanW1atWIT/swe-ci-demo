c43f73d75e480093bd26c3348fe93ca228394996
JordanW1atWIT <71137820+JordanW1atWIT@users.noreply.github.com>
2022-03-30 15:06:47 -0400

    Update FizzBuzz.java
    Updated Code

--- src/main/java/com/demo/FizzBuzz.java
+++ src/main/java/com/demo/FizzBuzz.java
@@ -3,60 +3,84 @@ package com.demo;
 public class FizzBuzz 
 {
     String processNumber(int num)
-	{	
-    	if(num == 0)
+    {
+        // Copy your code here
+          if(num == 0)
           {
-            return String.valueOf(0);  
+            return String.valueOf(0);
           }
           else if(num % 15 == 0)
           {
-            return "fizz buzz";
+            return "FizzBuzz";
           }
           else if(num % 5 == 0)
           {
-            return "buzz";
+            return "Buzz";
           }
           else if(num % 3 == 0)
           {
-            return "fizz";
+            return "Fizz";
           }
           else
           {
             return String.valueOf(num);
           }
-	}
-	
-	String processNumbers (int[] numList)
-	{
-    	String temp = "";
-    if(numList.length == 0)
-          {
-            return "invalid";
-          }
-        for(int i = 0; i < numList.length; i++)
+    }
+
+    String processNumbers (int[] nums)
+{
+            String temp = "";
+            if(nums.length == 0)
+              {
+                    return "invalid";
+              }
+        for(int i = 0; i < nums.length-1; i++)
         {
-          if(numList[i] == 0)
+          if(nums[i] == 0)
           {
-            temp += String.valueOf(0) + ",";  
+            temp += String.valueOf(0) + ", ";
           }
-          else if(numList[i] % 15 == 0)
+          else if(nums[i] % 15 == 0)
           {
-            temp += "fizz buzz,";
+            temp += "FizzBuzz, ";
           }
-          else if(numList[i] % 5 == 0)
+          else if(nums[i] % 5 == 0)
           {
-            temp += "buzz,";
+            temp += "Buzz, ";
           }
-          else if(numList[i] % 3 == 0)
+          else if(nums[i] % 3 == 0)
           {
-            temp += "fizz,";
+            temp += "Fizz, ";
           }
           else
           {
-            temp += String.valueOf(numList[i]) + ",";
+            temp += String.valueOf(nums[i]) + ", ";
           }
+
+
         }
+
+    if(nums[nums.length - 1] == 0)
+          {
+            temp += String.valueOf(0);
+          }
+          else if(nums[nums.length - 1] % 15 == 0)
+          {
+            temp += "FizzBuzz";
+          }
+          else if(nums[nums.length - 1] % 5 == 0)
+          {
+            temp += "Buzz";
+          }
+          else if(nums[nums.length - 1] % 3 == 0)
+          {
+            temp += "Fizz";
+          }
+          else
+          {
+            temp += String.valueOf(nums[nums.length - 1]);
+          }
         return temp;
-	}
+    }
 
 }
